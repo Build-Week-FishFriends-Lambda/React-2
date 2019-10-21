48d9bf526f547123b8e1307af830313cfdbdc2e2
Michael Harms <michael.harms6010@gmail.com>
2019-10-21 12:54:58 -0500

    built login and signup forms

---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => (
+        <div>
+          <Link to='/login'>Login</Link>
+          <Link to='/signup'>Signup</Link>
+        </div>
+);
+
+export default HomePage;
